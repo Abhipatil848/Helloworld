2bf086e28044a91b90f081b8b60b00bd8eaf5ccc
Abhishek <abhishekpatil5510@gmail.com>
2022-04-04 18:39:48 +0530

    Created Hello world

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,7 @@
-
 public class Helloworld{
     public static void main(String[] args) {
         System.out.println("Helloworld ");
-        System.out.println("Hii..!!My Name Is Abhishek..!!!");
+        System.out.println("Hello world...!!");
     }
 
 }
